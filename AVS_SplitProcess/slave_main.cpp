ebc3abb856c74c6413dcac64a19de3d7c216f817
SAPikachu <sapikachu@gmail.com>
2013-08-26 08:04:46 +0800

    Prevent plugins from output anything to stdout

--- AVS_SplitProcess/slave_main.cpp
+++ AVS_SplitProcess/slave_main.cpp
@@ -6,29 +6,42 @@
 #include "avisynth.h"
 #include "splitprocess.h"
 #include <memory>
+#include <assert.h>
+#include <io.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    HANDLE stdout_handle_original = GetStdHandle(STD_OUTPUT_HANDLE);
+    assert(stdout_handle_original != INVALID_HANDLE_VALUE);
+    HANDLE stdout_handle = INVALID_HANDLE_VALUE;
+    DuplicateHandle(GetCurrentProcess(), stdout_handle_original, GetCurrentProcess(), &stdout_handle, 0, FALSE, DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS);
+    assert(stdout_handle != INVALID_HANDLE_VALUE);
+    SetStdHandle(STD_OUTPUT_HANDLE, GetStdHandle(STD_ERROR_HANDLE));
+
+    auto stdout_file = _fdopen(_open_osfhandle((intptr_t)stdout_handle, _O_TEXT), "w");
+
     TCHAR script[32768];
     memset(script, 0, sizeof(script));
     if (!GetEnvironmentVariable(SCRIPT_VAR_NAME, script, ARRAYSIZE(script) - 1))
     {
-        printf("GetEnvironmentVariable failed, code = %d.\n", GetLastError());
+        fprintf(stdout_file, "GetEnvironmentVariable failed, code = %d.\n", GetLastError());
         return 1;
     }
     if (strlen(script) == 0)
     {
-        printf("Script is empty.\n");
+        fprintf(stdout_file, "Script is empty.\n");
         return 2;
     }
     HANDLE stdin_handle = GetStdHandle(STD_INPUT_HANDLE);
     if (!stdin_handle)
     {
-        printf("Standard input not redirected.\n");
+        fprintf(stdout_file, "Standard input not redirected.\n");
         return 5;
     } else if (stdin_handle == INVALID_HANDLE_VALUE) {
-        printf("GetStdHandle failed, code = %d.\n", GetLastError());
+        fprintf(stdout_file, "GetStdHandle failed, code = %d.\n", GetLastError());
         return 6;
     }
     IScriptEnvironment* env = CreateScriptEnvironment();
@@ -41,21 +54,21 @@ int _tmain(int argc, _TCHAR* argv[])
         }
         catch (IScriptEnvironment::NotFound)
         {
-            printf("Unexpected exception: Filter not found.\n");
+            fprintf(stdout_file, "Unexpected exception: Filter not found.\n");
             return 3;
         }
         catch (AvisynthError e)
         {
-            printf("Script error: %s\n", e.msg);
+            fprintf(stdout_file, "Script error: %s\n", e.msg);
             return 4;
         }
         catch (...)
         {
-            printf("Unknown exception\n");
+            fprintf(stdout_file, "Unknown exception\n");
             return 7;
         }
-        printf(SLAVE_OK_FLAG "\n");
-        fflush(stdout);
+        fprintf(stdout_file, SLAVE_OK_FLAG "\n");
+        fflush(stdout_file);
         char dummy_buffer[256];
         DWORD bytes_read = 0;
         do
@@ -66,7 +79,7 @@ int _tmain(int argc, _TCHAR* argv[])
                 break;
             }
         } while (bytes_read > 0);
-        printf("Terminating...\n");
+        fprintf(stdout_file, "Terminating...\n");
     }
     return 0;
 }
