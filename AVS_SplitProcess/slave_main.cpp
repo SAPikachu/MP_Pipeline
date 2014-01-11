0d4b29d1a34d515319a513f41b4c34a9ddbadc1d
SAPikachu <i@sapika.ch>
2014-01-11 17:06:44 +0800

    Try to silent all error dialogs on exit

--- AVS_SplitProcess/slave_main.cpp
+++ AVS_SplitProcess/slave_main.cpp
@@ -10,13 +10,78 @@
 #include <io.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <Psapi.h>
 
 #include "../AVS_SplitProcess/statement.h"
 
 typedef IScriptEnvironment* (__stdcall *CreateScriptEnvironment_t)(int);
 
+typedef unsigned int (__cdecl *_set_abort_behavior_t)(unsigned int _Flags, unsigned int _Mask);
+typedef int (__cdecl *_set_error_mode_t)(int _Mode);
+
 #define DLL_STATEMENT_START "^\\s*### dll:"
 #define DLL_STATEMENT_PARAM "\\s*(.*?)\\s*$"
+
+#define DISABLE_EXIT_HACK_STATEMENT "^\\s*### disable exit hack"
+
+LONG WINAPI exit_on_unhandled_exception(struct _EXCEPTION_POINTERS *ExceptionInfo) {
+    ExitProcess(255);
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+
+void error_box(char* format, ...) {
+    char message[16384];
+    memset(message, 0, sizeof(message));
+    va_list va;
+    va_start(va, format);
+    vsnprintf(message, sizeof(message)-1, format, va);
+    va_end(va);
+    MessageBoxA(NULL, message, "Message", MB_OK);
+}
+
+int silent_error_messages() {
+    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
+    SetUnhandledExceptionFilter(exit_on_unhandled_exception);
+    _set_abort_behavior(0, _CALL_REPORTFAULT | _WRITE_ABORT_MSG);
+    _set_error_mode(_OUT_TO_STDERR);
+
+    // Dirty hack to silent abort message box by other versions of VC runtime
+    DWORD bytes_needed = 0;
+    HMODULE modules[16384];
+    memset(modules, 0, sizeof(modules));
+    if (!EnumProcessModules(GetCurrentProcess(), modules, sizeof(modules), &bytes_needed)) {
+#ifdef _DEBUG
+        error_box("EnumProcessModules failed (%d)", GetLastError());
+#endif
+        return 1;
+    }
+    for (size_t i = 0; i < bytes_needed / sizeof(HMODULE); i++) {
+        char module_file_name[MAX_PATH + 1];
+        memset(module_file_name, 0, sizeof(module_file_name));
+        if (!GetModuleFileNameA(modules[i], module_file_name, MAX_PATH)) {
+#ifdef _DEBUG
+            error_box("GetModuleFileName failed (%d, %p)", GetLastError(), modules[i]);
+#endif
+            continue;
+        }
+        char* pos = strrchr(module_file_name, '\\');
+        char* name = pos ? pos + 1 : module_file_name;
+        if (_strnicmp(name, "msvcr", 5) != 0) {
+            // Not VC runtime
+            continue;
+        }
+        _set_abort_behavior_t sab = (_set_abort_behavior_t)GetProcAddress(modules[i], "_set_abort_behavior");
+        if (sab) {
+            sab(0, _CALL_REPORTFAULT | _WRITE_ABORT_MSG);
+        }
+
+        _set_error_mode_t sem = (_set_error_mode_t)GetProcAddress(modules[i], "_set_error_mode");
+        if (sem) {
+            sem(_OUT_TO_STDERR);
+        }
+    }
+    return 0;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -109,6 +174,13 @@ int _tmain(int argc, _TCHAR* argv[])
         } while (bytes_read > 0);
         fprintf(stdout_file, "Terminating...\n");
         fflush(stdout_file);
+
+#ifndef _DEBUG
+        // Sometimes filters have problem on destruction, try to silent them
+        if (!has_statement(script, DISABLE_EXIT_HACK_STATEMENT)) {
+            silent_error_messages();
+        }
+#endif
     }
     // It is not safe to delete env here since env is not allocated by us,
     // and we don't have env->DeleteScriptEnvironment on pre-2.6,
@@ -117,6 +189,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
     // To prevent problems we don't free it, since we are exiting anyways
     // FreeLibrary(avisynth_module);
+    ExitProcess(0);
     return 0;
 }
 
